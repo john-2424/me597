98266755a6b607b1a20c22c7cdb1de50b095aa07
Krishna <srajule@purdue.edu>
2025-09-10 17:07:58 -0400

    Lab 1: Task 1: Talker-Basic Publisher: c

---
+++ lab1_ws/src/task_1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'task_1'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='srajule',
+    maintainer_email='srajule@purdue.edu',
+    description='ME597 Lab 1 Task 1',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+		    'talker = task_1.basic_publisher:main',
+        ],
+    },
+)
